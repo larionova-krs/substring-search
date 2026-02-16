e203a4fcb278405f2a6bf9f23f2d788dc637ea25
Kristina Larionova <larionova.krs@yandex.ru>
2026-02-16 23:36:19 +0300

    feat: add Boyer-Moore-Horspool algorithm

--- subsearch/include/subsearch/exact.hpp
+++ subsearch/include/subsearch/exact.hpp
@@ -10,4 +10,7 @@ std::vector<Match> rabinKarp(const std::string& s, const std::string& pattern);
 std::vector<Match> rabinKarpNoChecking(const std::string& s,
                                        const std::string& pattern);
 
+std::vector<Match> boyerMooreHorspool(const std::string& s,
+                                      const std::string& pattern);
+
 }  // namespace subsearch::exact
--- subsearch/src/exact.cpp
+++ subsearch/src/exact.cpp
@@ -1,4 +1,5 @@
 #include <cctype>
+#include <unordered_map>
 #include <subsearch/exact.hpp>
 
 namespace subsearch::exact {
@@ -70,5 +71,52 @@ std::vector<Match> rabinKarpNoChecking(const std::string& s,
     }
     return matches;
 }
+
+std::vector<Match> boyerMooreHorspool(const std::string& s,
+                                      const std::string& pattern) {
+    std::vector<Match> matches;
+    int n = s.length();
+    int m = pattern.length();
+    if (m > n || pattern == "")
+        return matches;
+
+    std::unordered_map<char, int> charsLastOccuranceInPattern;
+    for (int i = 0; i < m; ++i) {
+        charsLastOccuranceInPattern[tolower(static_cast<unsigned char>(pattern[i]))] = i;
+    }
+    for (int i = m - 1; i < n; ++i) {
+        bool isOk = true;
+        for (int j = m - 1; j >= 0; --j) {
+            if (tolower(static_cast<unsigned char>(pattern[j])) !=
+                tolower(static_cast<unsigned char>(s[i - (m - 1 - j)]))) {
+                isOk = false;
+                if (!charsLastOccuranceInPattern.contains(tolower(
+                        static_cast<unsigned char>(s[i - (m - 1 - j)])))) {
+                    i += j;
+                } else {
+                    i += m -
+                         charsLastOccuranceInPattern.at(tolower(
+                             static_cast<unsigned char>(s[i - (m - 1 - j)]))) - 2;
+                }
+                break;
+            }
+        }
+        if (isOk) {
+            matches.push_back(Match(i - m + 1, m, &s));
+        }
+    }
+    return matches;
+}
+
+// std::vector<Match> boyerMoore(const std::string& s,
+//                                       const std::string& pattern) {
+//     std::vector<Match> matches;
+//     int n = s.length();
+//     int m = pattern.length();
+//     if (m > n)
+//         return matches;
+
+//     return matches;
+// }
 
 }  // namespace subsearch::exact
