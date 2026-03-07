45f668beb4a245d77184828984f7beba9a9aa182
Kristina Larionova <larionova.krs@yandex.ru>
2026-03-08 00:09:56 +0300

    feat: add Sellers fuzzy search algorithm

--- subsearch/src/fuzzy.cpp
+++ subsearch/src/fuzzy.cpp
@@ -44,4 +44,57 @@ int wagnerFischerLevenshteinDist(const std::string& s1, const std::string& s2, i
     return dp[m][n];
 }
 
+void fillMatches(std::vector<Match>& matches, const std::string& text, const std::string& pattern, const std::vector<std::vector<int>>& dp, int threshold, int insertCost, int deleteCost, int replaceCost) {
+    int m = pattern.size();
+    int n = text.size();
+
+    for (int j_end = 1; j_end <= n; ++j_end) {
+        if (dp[m][j_end] > threshold)
+            continue;
+
+        int i = m, j = j_end;
+        while (i > 0) {
+            if (j > 0) {
+                if (dp[i][j] == dp[i - 1][j - 1] || dp[i][j] == dp[i - 1][j - 1] + replaceCost) {
+                    --i, --j;
+                } else if (dp[i][j] == dp[i - 1][j] + deleteCost) {
+                    --i;
+                } else {
+                    --j;
+                }
+            } else {
+                --i;
+            }
+        }
+
+        int matchLen = j_end - j;
+        int dist = dp[m][j_end];
+        int maxDist = m * deleteCost + matchLen * insertCost;
+        double similarity = (1.0 - static_cast<double>(dist) / maxDist) * 100.0;
+        matches.emplace_back(j, matchLen, &text, similarity);
+    }
+}
+
+std::vector<Match> sellersSearch(const std::string& text, const std::string& pattern, int threshold, int insertCost, int deleteCost, int replaceCost) {
+    std::vector<Match> matches;
+    int m = pattern.size();
+    int n = text.size();
+
+    if (m == 0)
+        return matches;
+
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+    for (int i = 1; i <= m; ++i) {
+        dp[i][0] = dp[i - 1][0] + deleteCost;
+
+        for (int j = 1; j <= n; ++j) {
+            dp[i][j] = (pattern[i - 1] == text[j - 1]) ? dp[i - 1][j - 1] : std::min(dp[i - 1][j] + deleteCost, std::min(dp[i][j - 1] + insertCost, dp[i - 1][j - 1] + replaceCost));
+        }
+    }
+
+    fillMatches(matches, text, pattern, dp, threshold, insertCost, deleteCost, replaceCost);
+    
+    return matches;
+}
+
 }  // namespace subsearch::fuzzy
