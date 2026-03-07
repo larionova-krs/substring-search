f6163ffcc0037e6904d98d0304d6dab89978d474
Kristina Larionova <larionova.krs@yandex.ru>
2026-03-07 18:10:38 +0300

    feat: add Wagner-Fischer algorithm for Levenshtein distance

--- subsearch/include/subsearch/fuzzy.hpp
+++ subsearch/include/subsearch/fuzzy.hpp
@@ -8,4 +8,6 @@ namespace subsearch::fuzzy {
 
 std::vector<Match> naiveFuzzySearch(const std::string& text, const std::string& pattern, double threshold = 70.0);
 
+int wagnerFischerLevenshteinDist(const std::string& s1, const std::string& s2, int insertCost = 1, int deleteCost = 1, int replaceCost = 1);
+
 }  // namespace subsearch::fuzzy
--- subsearch/src/fuzzy.cpp
+++ subsearch/src/fuzzy.cpp
@@ -25,4 +25,23 @@ std::vector<Match> naiveFuzzySearch(const std::string& text, const std::string& 
     return matches;
 }
 
+int wagnerFischerLevenshteinDist(const std::string& s1, const std::string& s2, int insertCost, int deleteCost, int replaceCost) {
+    int m = s1.size();
+    int n = s2.size();
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+    // dp[0][0] = 0;
+
+    for (int j = 1; j <= n; ++j) {
+        dp[0][j] = dp[0][j - 1] + insertCost;
+    }
+    for (int i = 1; i <= m; ++i) {
+        dp[i][0] = dp[i - 1][0] + deleteCost;
+
+        for (int j = 1; j <= n; ++j) {
+            dp[i][j] = (s1[i - 1] == s2[j - 1]) ? dp[i - 1][j - 1] : std::min(dp[i - 1][j] + deleteCost, std::min(dp[i][j - 1] + insertCost, dp[i - 1][j - 1] + replaceCost));
+        }
+    }
+    return dp[m][n];
+}
+
 }  // namespace subsearch::fuzzy
